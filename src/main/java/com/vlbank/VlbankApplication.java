1cdb0c1934f926764f6a964cadae08d1580ab378
Victor Diego <studyvictordiego@gmail.com>
2025-02-14 02:04:47 -0300

    Initial

---
+++ src/main/java/com/vlbank/VlbankApplication.java
@@ -0,0 +1,13 @@
+package com.vlbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlbank/VlbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
