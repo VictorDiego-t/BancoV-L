875d9c17003a67cd40defd291b79b52ac56de19d
Victor Diego <studyvictordiego@gmail.com>
2025-02-14 05:41:28 -0300

    Correção Bugs TransactionService

---
+++ src/main/java/com/vlbank/services/TransactionService.java
@@ -0,0 +1,44 @@
+package com.vlbank.services;
+
+import com.vlbank.domain.Transaction.Transaction;
+import com.vlbank.domain.user.User;
+import com.vlbank.dtos.TransactionDTO;
+import com.vlbank.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transaction.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        this.repository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+
+        return newTransaction;
+
+   }
+}
---
+++ src/test/java/com/vlbank/domain/user/UserTest.java
@@ -0,0 +1,84 @@
+package com.vlbank.domain.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void setBalance() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getDocument() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void getUserType() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void setDocument() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void setUserType() {
+    }
+}
