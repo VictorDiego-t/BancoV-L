b800e0e678ee8933cdb396c28be228d29e2f1f9f
Victor Diego <studyvictordiego@gmail.com>
2025-02-14 02:34:07 -0300

    Criação do Domain User

---
+++ src/main/java/com/vlbank/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.vlbank.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Vai gerar um Id Automatico
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true) // Define a coluna document como unica
+    private String document;
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
